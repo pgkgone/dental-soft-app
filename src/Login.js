3a3b9456c22f0e6fdb0913474fb624266fb20d52
Megatron997 <Megatron997@mail.ru>
2019-12-12 19:55:30 +0300

    Login

---
+++ src/Login.js
@@ -0,0 +1,140 @@
+import React, { Component } from "react";
+import {
+  Container,
+  Header,
+  Left,
+  Body,
+  Right,
+  Button,
+  Icon,
+  Title,
+  Text,
+  Content,
+  Form,
+  Item,
+  Input,
+  Label,
+} from "native-base";
+import { Alert, StyleSheet, View } from "react-native";
+import * as FileSystem from "expo-file-system";
+import * as Font from "expo-font";
+import Constants from "expo-constants";
+import * as SecureStore from 'expo-secure-store';
+export class Login extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  state = {
+    loading: true,
+    username: "reg",
+    password: "111"
+  };
+
+  async componentDidMount() {
+    await Font.loadAsync({
+      Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf")
+    });
+    this.setState({ loading: false });
+  }
+
+  render() {
+    if (this.state.loading) {
+      return <View></View>;
+    } else {
+      return (
+        <Container>
+          <Header style={this.styles.header}>
+            <Left style={{ flex: 1 }}></Left>
+            <Body style={{ flex: 1 }}>
+              <Title style={this.styles.title}>Авторизация</Title>
+            </Body>
+            <Right style={{ flex: 1 }}></Right>
+          </Header>
+          <Content style={this.styles.content}>
+            <Form>
+              <Item floatingLabel>
+                <Label>Username</Label>
+                <Input
+                  onChangeText={v => this.setState({ username: v })} //(v)=>this.setState({"username":v}
+                  value={this.state.username}
+                />
+              </Item>
+              <Item floatingLabel>
+                <Label>Password</Label>
+                <Input value={this.state.password} secureTextEntry />
+              </Item>
+              <Button
+                transparent
+                last
+                style={this.styles.loginBtn}
+                onPress={async () => {
+                  var url =
+                    "https://online.dental-soft.ru/docs_test.php?ID=demo&doc_name_demo=" +
+                    this.state.username +
+                    "&doc_pass_demo=" +
+                    this.state.password;
+                  let data = await fetch(url, {
+                    method: "POST"
+                  }).then(response => response.text())
+                  .catch( err => {
+                    Alert.alert(
+                        "Ошибка авторизации",
+                        "Нет соединения с сервером!",
+                        [
+                          { text: "OK", onPress: () => console.log("OK Pressed") }
+                        ],
+                        { cancelable: false }
+                      );
+                  });
+
+                  console.log(data.split(":"));
+                  if (data.includes("неправильный логин или пароль")) {
+                    Alert.alert(
+                      "Ошибка авторизации",
+                      "Неправильный логин или пароль",
+                      [
+                        { text: "OK", onPress: () => console.log("OK Pressed") }
+                      ],
+                      { cancelable: false }
+                    );
+                  } else {
+                    await SecureStore.setItemAsync("data", data)
+                    console.log(await SecureStore.getItemAsync("data"))
+                    //filename = (await FileSystem.documentDirectory) + "tempname" + ".txt";
+                    //FileSystem.writeAsStringAsync(filename , data)
+                    ///var rez = await FileSystem.readAsStringAsync(filename);
+                    //console.log(rez);
+                  }
+                }}
+              >
+                <Text style={this.styles.loginText}>LOGIN</Text>
+              </Button>
+            </Form>
+          </Content>
+        </Container>
+      );
+    }
+  }
+
+  styles = StyleSheet.create({
+    header: {
+      marginTop: Constants.statusBarHeight,
+      backgroundColor: "#A52B2A"
+    },
+    title: {
+      color: "#ffffff"
+    },
+    content: {
+      backgroundColor: "#F1FFF0"
+    },
+    loginBtn: {
+      marginTop: 12,
+      alignSelf: "center"
+    },
+    loginText: {
+      fontSize: 15
+    }
+  });
+}
+module.exports = Login;
--- src/Main.js
+++ src/Main.js
@@ -1,120 +1,68 @@
-import React, { Component } from "react";
-import {
-  Container,
-  Header,
-  Left,
-  Body,
-  Right,
-  Button,
-  Icon,
-  Title,
-  Text,
-  Content
-} from "native-base";
-import { Alert, StyleSheet, FlatList } from "react-native";
-import * as FileSystem from "expo-file-system";
-import * as Font from "expo-font";
-import Constants from 'expo-constants';
-import flatListData from '../data/flatListData'
-
+import React from "react";
+import { Text } from "react-native";
+import * as soap from "soap-everywhere";
+import * as xml from 'react-native-xml2js'
+import * as xml2 from 'react-xml-parser'
 export class Main extends React.Component {
   state = {
-    data: null,
-    loading: true,
-    currentDate: this.getDate()
+    data: null
   };
 
-  getDate(){
-    var today = new Date(); 
-    var dd = today.getDate(); 
-    var mm = today.getMonth() + 1; 
-    var yyyy = today.getFullYear(); 
-    if (dd < 10) { 
-        dd = '0' + dd; 
-    } 
-    if (mm < 10) { 
-        mm = '0' + mm; 
-    } 
-    return dd + '.' + mm + '.' + yyyy; 
-  }
+  /*async componentDidMount()
+    {
+        let data = await fetch('https://online.dental-soft.ru/docs_test.php?ID=demo&doc_name_demo=reg&doc_pass_demo=111');
+        let response = await data.text();
+        this.setState({data : response});
+    }*/
 
-  // only for DEMO CLINIC
-  async login(login, pass) {
-    var url =
-      "https://online.dental-soft.ru/docs_test.php?ID=demo&doc_name_demo=" +
-      login +
-      "&doc_pass_demo=" +
-      pass;
-    let data = await fetch(url, {
-      method: "POST"
-    }).then(response => response.text());
+  /*splitStringIntoWords(str){
+        console.log('ok boomer');
+        let strSplitted = str.split(":");
+        var url = 'http://vds.dental-soft.ru:2102';
+        var args = 
+            {
+                tokenId: '555',
+                dic_id: 1
+            };
+        soap.createClient(url, function(err, client) 
+        {
+            client.GetDatesAll (args, function(err, result)
+                {
+                    console.log(result);
+                }
+            );
+        });
+*/
 
-    console.log(data.split(":"));
-    if (data.includes("неправильный логин или пароль")) {
-      Alert.alert(
-        "Ошибка авторизации",
-        "Неправильный логин или пароль",
-        [{ text: "OK", onPress: () => console.log("OK Pressed") }],
-        { cancelable: false }
-      );
-    } else {
-      //filename = (await FileSystem.documentDirectory) + "tempname" + ".txt";
-      //FileSystem.writeAsStringAsync(filename , data)
-      ///var rez = await FileSystem.readAsStringAsync(filename);
-      //console.log(rez);
-    }
-  }
   async componentDidMount() {
-    await Font.loadAsync({
-      Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf")
-    });
-    this.setState({ loading: false });
-
-    this.login("alex", "123");
-    console.log("!!json!!!");
+    console.log("!!json!!!")
+    //var url = "http://vds.dental-soft.ru:2102/?wsdl";
+    
+    /*let data = await fetch(url, {
+      method: "POST",
+      body: '<Envelope xmlns="http://www.w3.org/2003/05/soap-envelope"><Body> <GetDates xmlns="urn:grvssl"><tokenId>555</tokenId><docId>1</docId></GetDates></Body></Envelope>'
+    })
+      .then(
+        response => response.text()
+      )
+      .then(html => html);
+*/
     //var url = "https://webhook.site/fc9d04fe-1695-4cac-9e88-28c77a0d742e";
     var url = "http://vds.dental-soft.ru:2102/?wsdl";
-    var __xmlattr = "<tokenId>555</tokenId><docId>1</docId>";
-    var args = { tokenId: "555", docId: 1 };
+    var __xmlattr = '<tokenId>555</tokenId><docId>1</docId>';
+    var args = {tokenId : '555', docId : 1};
+    soap.createClient(url, function(err, client) {
+        client.GetDates({_xml: __xmlattr}, function(err, result) {
+            console.log(result);
+        })
+  });
+  }
 
-    //soap.createClient(url, function(err, client) {
-    //    client.GetDates({_xml: __xmlattr}, function(err, result) {
-    //        console.log(result);
-    //    })
-    //});
-  }
-  
   render() {
-    if (this.state.loading) {
-      return <Text>11</Text>;
+    if (this.state.data === null) {
+      return <Text> Loading </Text>;
     } else {
-      return (
-        <Container>
-          <Header style={this.styles.container}>
-            <Left style={{flex:1}}>
-              <Button transparent>
-                <Icon name="arrow-back" />
-              </Button>
-            </Left>
-            <Body style={{flex:1}}>
-              <Title>{this.state.currentDate}</Title>
-            </Body>
-            <Right style={{flex:1}}>
-              <Button transparent>
-                <Icon name="arrow-forward" />
-              </Button>
-            </Right>
-          </Header>
-          <Content>
-          </Content>
-        </Container>
-      );
+      return <Text> {this.state.data} </Text>;
     }
   }
-  
-  styles = StyleSheet.create({
-    container: {
-      marginTop: Constants.statusBarHeight,
-    }
-  })
 }
